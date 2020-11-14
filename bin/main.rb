36bda9693f8df03755b9ea1e0074988e3048a219
Your Name <you@example.com>
2020-11-14 16:58:22 +0100

    Fixing rspec problem, not to show part of the program

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/job'
+require_relative '../lib/first'
 
 def job_list
   puts ''
@@ -13,9 +13,23 @@ def job_list
 end
 job_list
 input = gets.chomp
-job3 = Job.new('https://weworkremotely.com/')
-job3.calling_list if input == 'list'
-job3.calling_list if input == 'LIST'
-job4 = Job.new('https://weworkremotely.com/')
-job4.calling_list if @answer2 == 'y'
-puts 'Goodbye, explore our job list again!'
+while input.include?('list') || input.include?('LIST')
+  job3 = First.new
+  a = job3.job_listing
+  a.each_with_index do |job, index|
+    puts "#{index}. #{job[:title]}"
+  end
+
+  puts 'Choose an index to see link for more details'
+  answer = gets.chomp
+  break unless answer.to_i.to_s == answer
+
+  puts "https://weworkremotely.com#{job3.job_list[answer.to_i][:grab_url]}"
+  puts 'Type yes if you want another search'
+  puts 'Type no if you want to exit the program'
+
+  answer1 = gets.chomp
+  break if answer1 == 'no'
+
+end
+puts 'Explore our job list again! Goodbye!'
