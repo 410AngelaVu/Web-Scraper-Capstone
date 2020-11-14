bab8222e8bd3e1b6f7b47abb2308331383a8408c
410AngelaVu <65039720+410AngelaVu@users.noreply.github.com>
2020-11-14 17:32:48 +0100

    Update scraper_spec.rb

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,21 +1,10 @@
-require_relative '../lib/job'
-describe Job do
-  let(:job) { Job.new('https://weworkremotely.com/') }
-  describe '#calling_list' do
-    it 'shows full job list' do
-      expect(job.calling_list).to eql(nil)
-    end
-  end
-
+require_relative '../lib/first'
+describe First do
+  let(:job) { First.new }
   describe '#job_listing' do
     it 'returns array of jobs' do
       expect(job.job_listing).to be_an Array
     end
   end
-
-  describe '#full_job_list' do
-    it 'shows index of a job' do
-      expect(job.full_job_list(job.job_list)).to be_an Array
-    end
-  end
 end
+
